0398a6ebd929f1b5fd83f84c6594937b06e91da3
Martins <victorcostamartins0@gmail.com>
2025-03-11 17:40:47 -0300

    Funçoes fech - array, assoc, row e object

--- aulas/mysqli/concao.php
+++ aulas/mysqli/concao.php
@@ -7,18 +7,69 @@
 
     $conexao = mysqli_connect($servidor, $user, $pass, $banco) or die ("Não foi possivel fazer a conexão com o banco de dados");
 
-    $qry = "INSERT INTO cliente(cliente, email, fone, endereco) VALUE('raimundo', 'raimundo@gmail.com', '99-777777777', 'rua 9')";
+    $qryInsert = "INSERT INTO cliente(cliente, email, fone, endereco) VALUE('raimundo', 'raimundo@gmail.com', '99-777777777', 'rua 9')";
 
     $qryUpdate = "UPDATE cliente set endereco = 'Bairro de Lurdes' WHILE id_clinete = 2";
-
     $qryDelete = "DELETE FROM `cliente` WHERE id_cliente = 2";
+    
+    $qry = "SELECT * FROM cliente";
 
-    $resultado = mysqli_query($conexao, $qryDelete);
+    // $resultado = mysqli_query($conexao, $qry);
+    
+    $geral = mysqli_fetch_array( mysqli_query($conexao, $qry));
 
-    if($resultado) {
-        echo "Excluido com sucesso";
-    }else {
-        echo "Não foi possivel  excluido ";
-    }
+    $associativo = mysqli_fetch_array(mysqli_query($conexao, $qry), MYSQLI_ASSOC);
+    $numerico = mysqli_fetch_array(mysqli_query($conexao, $qry), MYSQLI_NUM);
 
+        //mysqli_fetch_assoc - mysqli_fetch_row
+    $assoc = mysqli_fetch_assoc(mysqli_query($conexao, $qry));
+    $row = mysqli_fetch_row(mysqli_query($conexao, $qry));
+
+    $obj = mysqli_fetch_object(mysqli_query($conexao, $qry));
+
+    echo "mysqli_fetch_array - geral";
+    echo "<pre>";
+    print_r($geral);
+    echo "</pre>";
+
+    echo "<hr>";
+
+    echo "mysqli_fetch_array - numerico";
+    echo "<pre>";
+    print_r($numerico);
+    echo "</pre>";
+
+    echo "<hr>";
+
+    echo "mysqli_fetch_array - associativo";
+    echo "<pre>";
+    print_r($associativo);
+    echo "</pre>";
+
+    echo "<hr>";
+
+    echo "mysqli_fetch_array - assoc";
+    echo "<pre>";
+    print_r($assoc);
+    echo "</pre>";
+
+    echo "<hr>";
+
+    echo "mysqli_fetch_array - row";
+    echo "<pre>";
+    print_r($row);
+    echo "</pre>";
+
+    echo "<hr>";
+
+    echo "mysqli_fetch_array - object";
+    echo "<pre>";
+    print_r($obj);
+    echo "</pre>";
+
+    echo "<hr>";
+
+    echo "Pegando elemento Assoc = " .$assoc ["cliente"] ."<br>";
+    echo "Pegando elemento Row = " .$row [1] ."<br>";
+    echo "Pegando elemento do Object = " .$obj -> cliente;
 ?>
