8bdf77052a9da080555f1c7a68bca2c3b1ae12ef
Martins <victorcostamartins0@gmail.com>
2025-04-20 23:15:14 -0300

    listando nome e excluindo item

--- PHP-OO/Sis-Bancario/pdo.php
+++ PHP-OO/Sis-Bancario/pdo.php
@@ -13,6 +13,8 @@
 
     // $ferramentas = $sql->fetchAll();
 
+    // $ferramentas = $sql->fetchAll(PDO::FETCH_OBJ);
+    // $ferramenta = $ferramentas[0];
     
     // $conn->exec("INSERT INTO usuario (usuario, email, senha) VALUES ('euDuasVezes','edu@gmail.com', '123')");
     $listUser = $conn->prepare("SELECT * FROM usuario");
@@ -21,8 +23,17 @@
     $usuarios = $listUser->fetchAll(PDO::FETCH_OBJ);
     
 
-    $ferramentas = $sql->fetchAll(PDO::FETCH_OBJ);
-    // $ferramenta = $ferramentas[0];
+    $returnUsar = $conn->prepare("SELECT usuario,email,senha FROM usuario");
+    $returnUsar->execute();
+
+    $ret = $returnUsar->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach($ret as $linha) {
+        echo $linha["usuario"] ."<br>";
+    };
+
+    $delete = "DELETE FROM usuario WHERE id_usuario=21";
+    $conn->exec($delete);
 
 
     echo "<pre>";
@@ -32,4 +43,6 @@
         // var_dump($ferramenta);
         // var_dump($ferramenta->ferramenta);
 
-        var_dump($usuarios);
+        // var_dump($usuarios);
+
+        // var_dump($ret);
